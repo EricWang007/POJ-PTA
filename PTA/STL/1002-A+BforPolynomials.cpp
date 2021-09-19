36784adb6d2bb90c56fbddae48670870abc09d55
EricWang <2500549856@qq.com>
2021-09-19 21:48:19 +0800

    Update 1002-A+BforPolynomials.cpp

--- PTA/STL/1002-A+BforPolynomials.cpp
+++ PTA/STL/1002-A+BforPolynomials.cpp
@@ -1,44 +1,26 @@
-#include<iostream>
-#include<list>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-struct poly {
-    int ex;
-    double co;
-};
-
-int main(){
-    int k;
-    scanf("%d", &k);
-    list<poly> lst;    
-    while (k--) {
-        poly tmp;
-        scanf("%d %lf", &tmp.ex, &tmp.co);
-        lst.push_back(tmp);
+int main() {
+    map<int, double, greater<int>> p1;
+    int n1, n2;
+    scanf("%d", &n1);
+    while (n1--) {
+        int e;
+        double c;
+        scanf("%d %lf", &e, &c);
+        p1[e] = c;
     }
-    scanf("%d", &k);
-    list<poly>::iterator i = lst.begin();
-    
-    while (k--) {
-        poly tmp;
-        scanf("%d %lf", &tmp.ex, &tmp.co);
-        while (i !=lst.end() && tmp.ex < (*i).ex) i++;
-        if (i == lst.end()) lst.insert(i, tmp);//在i之前插入，i仍然指向插入的元素之后的元素
-        else {
-            if (tmp.ex > (*i).ex) lst.insert(i, tmp);
-            else if (tmp.ex == (*i).ex) {
-                double sum = tmp.co + (*i).co;
-                if (sum == 0) i=lst.erase(i); //此步容易错,需要把i重新赋值，不然i还指向删除的元素
-                else (*i).co = sum;
-            }
-        }
+    scanf("%d", &n2);
+    while (n2--) {
+        int e;
+        double c;
+        scanf("%d%lf", &e, &c);
+        p1[e] += c;
+        if (p1[e] == 0) p1.erase(e);
     }
-    printf("%d", lst.size());
-    i = lst.begin();
-    while(i != lst.end()) {
-        printf(" %d %.1lf", (*i).ex, (*i).co);
-        i++;
-    }
+    printf("%d", p1.size());
+    for (auto it : p1)
+        printf(" %d %.1lf", it.first, it.second);
     return 0;
 }
